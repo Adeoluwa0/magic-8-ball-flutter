07516e364382663439a54b1cd9ee90cc9d34f417
Adeoluwa <apata.adeoluwa@gmail.com>
2024-12-24 11:50:41 +0000

    A decisions App

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,57 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+          home: Scaffold(
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+        ),
+        body: BallPage(),
+      )),
+    );
+
+class BallPage extends StatelessWidget {
+  //const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Ball();
+  }
+}
+
+class Ball extends StatefulWidget {
+  //const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballnumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      ballnumber = Random().nextInt(4) + 1;
+                    });
+                  },
+                  child: Image.asset('images/ball$ballnumber.png'),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
+  }
+}
